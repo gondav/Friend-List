415002bde880f3fba570aad36829fda1ed6b2297
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-10-09 17:29:04 +0200

    add seeders

---
+++ backend/src/seeders/food.ts
@@ -0,0 +1,17 @@
+export const food = [
+  {
+    name: 'Tuscan chicken'
+  },
+  {
+    name: "Shepherd's pie"
+  },
+  {
+    name: 'Banana bread'
+  },
+  {
+    name: 'Pancake'
+  },
+  {
+    name: 'Lasagna'
+  }
+];
---
+++ backend/src/seeders/friends.ts
@@ -0,0 +1,44 @@
+export const friends = [
+  {
+    name: 'Gabby Mary Jane',
+    email: 'rufforouge@omtecha.com',
+    comment: '',
+    favFoodId: 5,
+    relationshipStatusId: 1
+  },
+  {
+    name: 'Cheryll Rosalyn',
+    email: 'nataliyaukr@saxophonexltd.com',
+    comment: 'Truly excellent!!',
+    favFoodId: 5,
+    relationshipStatusId: 3
+  },
+  {
+    name: 'Artie Collyn',
+    email: 'momebijo@netveplay.com',
+    comment: 'Let me take a nap... great shot, anyway.',
+    favFoodId: 2,
+    relationshipStatusId: 1
+  },
+  {
+    name: 'Booker Jarod',
+    email: 'bokkerjar1818@btcmod.com',
+    comment: 'Whoa.',
+    favFoodId: 3,
+    relationshipStatusId: 1
+  },
+  {
+    name: 'Martin Daffodil',
+    email: '1dieter@asifboot.com',
+    comment: '',
+    favFoodId: 1,
+    relationshipStatusId: 2
+  },
+  {
+    name: 'Karson Tory',
+    email: 'ballaswyn3699871@vemail.site',
+    comment: 'Clean. Love the use of gradient and shade!',
+    favFoodId: 5,
+    relationshipStatusId: 1
+  }
+];
