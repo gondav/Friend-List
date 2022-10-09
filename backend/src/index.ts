4eecfac073ddf3c55dcd62732d841fe59a7e33b8
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-10-09 14:40:15 +0200

    db imported and call checkConnection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import app from './app';
 import dotenv from 'dotenv';
+import { db } from './data/database';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+db.checkConnection();
 
 app.listen(PORT, () => console.log('Server started on port', PORT));
