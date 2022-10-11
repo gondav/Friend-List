7067f95c39bd027d52a4217db9dcbdf69e3ea138
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-10-11 22:38:45 +0200

    getFriendWithAllAttributes method added

--- backend/src/controller/friendController.ts
+++ backend/src/controller/friendController.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { badRequestError } from '../services/errorCreatorService';
 import { friendService } from '../services/friendService';
 
 export const friendController = {
@@ -9,5 +10,20 @@ export const friendController = {
   ) {
     const friends = await friendService.getFriendsWithAllAttributes();
     res.status(200).json({ friendList: friends });
+  },
+
+  async getFriendWithAllAttributes(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const friendId = Number(req.params.id);
+
+    if (isNaN(friendId) || friendId < 1) {
+      return next(badRequestError('Friend id needs to be a positive integer'));
+    }
+    const friend = await friendService.getFriendWithAllAttributes(friendId);
+
+    res.status(200).json({ friend });
   }
 };
