1924efa7017fde006f8bc18262290b4df8b1eb85
gondav <viktoria.gonda@gmail.com>
2022-10-13 17:48:53 +0200

    getFriendList method created

--- frontend/src/app/services/friend-service/friend.service.ts
+++ frontend/src/app/services/friend-service/friend.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { IFriendListResponseModel } from '../../shared/models/responses/IFriendListResponseModel';
+import { IFriendListViewModel } from '../../shared/models/viewmodels/IFriendListViewModel';
+import { environment } from '../../../environments/environment';
+import { BaseHttpService } from '../base-service/base.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FriendService {
+  constructor(private baseHttpService: BaseHttpService) {}
 
-  constructor() { }
+  getFriendList(): Observable<IFriendListViewModel[]> {
+    return this.baseHttpService
+      .getItems<IFriendListResponseModel>(environment.friendEndpoint)
+      .pipe(map((response) => response.friendList));
+  }
 }
