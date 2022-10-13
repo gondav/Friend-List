3c7af87f1f8d033f619d2369098ab62ddf576f22
gondav <viktoria.gonda@gmail.com>
2022-10-13 17:46:32 +0200

    using cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import errorHandler from './middlewares/errorHandler';
 import { friendRouter } from './routes/friend.routes';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', friendRouter);
 
