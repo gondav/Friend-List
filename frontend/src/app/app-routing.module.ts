6ab4f2039b939f2809321e1c3fc1cc1a31bb2ccb
gondav <viktoria.gonda@gmail.com>
2022-10-14 18:05:26 +0200

    set up landing page routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'friends', pathMatch: 'full' },
+  {
+    path: 'friends',
+    loadChildren: () =>
+      import('../app/features/landing-page/landing-page.module').then(
+        (m) => m.LandingPageModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
