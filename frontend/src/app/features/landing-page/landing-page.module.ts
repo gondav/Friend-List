246fa7082cfd6de66ee3473939392c545caee685
gondav <viktoria.gonda@gmail.com>
2022-10-14 20:27:39 +0200

    FriendCardComponent created

--- frontend/src/app/features/landing-page/landing-page.module.ts
+++ frontend/src/app/features/landing-page/landing-page.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 import { LandingPageComponent } from './landing-page.component';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
+import { FriendCardComponent } from './friend-card/friend-card.component';
 
 @NgModule({
-  declarations: [LandingPageComponent],
+  declarations: [LandingPageComponent, FriendCardComponent],
   imports: [CommonModule, SharedModule, LandingPageRoutingModule],
 })
 export class LandingPageModule {}
