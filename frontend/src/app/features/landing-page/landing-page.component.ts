2eff3a4e0a21315f4a121013f180b3b1db725e0c
gondav <viktoria.gonda@gmail.com>
2022-10-15 15:38:44 +0200

    add subscription to friendService.getFriendList method in landing page

--- frontend/src/app/features/landing-page/landing-page.component.ts
+++ frontend/src/app/features/landing-page/landing-page.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FriendService } from '../../services/friend-service/friend.service';
+import { IFriendViewModel } from '../../shared/models/viewmodels/IFriendViewModel';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  friends: IFriendViewModel[] = [];
 
-  constructor() { }
+  constructor(private friendService: FriendService) {}
 
   ngOnInit(): void {
+    this.friendService.getFriendList().subscribe({
+      next: (friendList) => {
+        this.friends = friendList;
+      },
+      error: (error) => console.log(error),
+    });
   }
-
 }
