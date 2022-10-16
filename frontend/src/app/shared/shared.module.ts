7b0c03630426e5276b2332ecc474561ae487040f
gondav <viktoria.gonda@gmail.com>
2022-10-16 12:12:10 +0200

    add BackgroundImgDirective to declarations and exports in Shared module

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { RelationshipStatusPipe } from './pipes/relationshipStatus.pipe';
+import { BackgroundImgDirective } from './directives/background-img.directive';
 
 @NgModule({
-  declarations: [HeaderComponent, RelationshipStatusPipe],
+  declarations: [
+    HeaderComponent,
+    RelationshipStatusPipe,
+    BackgroundImgDirective,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, RelationshipStatusPipe],
+  exports: [HeaderComponent, RelationshipStatusPipe, BackgroundImgDirective],
 })
 export class SharedModule {}
