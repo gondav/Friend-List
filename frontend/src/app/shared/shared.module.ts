26a72f804e12ecf496edcd8b6c22842ca468a8c9
gondav <viktoria.gonda@gmail.com>
2022-10-16 11:16:12 +0200

    add RelationshipStatusPipe to SharedModule declarations and exports

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { RelationshipStatusPipe } from './pipes/relationshipStatus.pipe';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, RelationshipStatusPipe],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, RelationshipStatusPipe],
 })
 export class SharedModule {}
